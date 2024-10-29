8649a4b36151e946232c4d34b06bd7e16267c74b
aryansareen <aryansareendj@gmail.com>
2024-10-29 21:56:45 +0000

    Update app.py to set correct port for Render deployment

--- app/app.py
+++ app/app.py
@@ -77,7 +77,9 @@ def index():
 if __name__ == "__main__":
     # Start the scheduler in a separate thread
     import threading
-    threading.Thread(target=schedule.run_pending, daemon=True).start()
-    app.run(debug=True)
-
-    
+    scheduler_thread = threading.Thread(target=run_scheduler, daemon=True)
+    scheduler_thread.start()
+   # Use the port from the environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+    git add .
